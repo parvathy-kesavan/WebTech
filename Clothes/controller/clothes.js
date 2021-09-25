c43356efa008e52d1d4d042fbb83435519db928a
Arjun Suraj Laguduva Rameshbabu <aslaguduvarameshbabu1@sheffield.ac.uk>
2021-09-25 11:37:26 +0100

    change

--- Clothes/controller/clothes.js
+++ Clothes/controller/clothes.js
@@ -1,21 +1,23 @@
 const clothes = require('../model/clothes.js');
+const user_signup = require('../model/user_signup.js');
+
 
 
 exports.cloth = (req, res) => {
-    // Request validation
+
     if(!req.body) {
         
         return res.status(400).send({
             message: "Clothes can not be empty"
         });
     }
-    // Create a Product
+
     const cloth = new clothes({
         title: req.body.title || "No cloth title", 
         description: req.body.description,
         price: req.body.price,
     });
-    // Save Product in the database
+
     cloth.save()
     .then(data => {
         res.send(data);
@@ -26,7 +28,7 @@ exports.cloth = (req, res) => {
     });
 };
 
-// Retrieve all products from the database.
+
 exports.findAll = (req, res) => {
     clothes.find()
     .then(c => {
@@ -39,3 +41,27 @@ exports.findAll = (req, res) => {
     });
 };
 
+exports.signup = (req, res) => {
+  
+    if(!req.body) {
+        
+        return res.status(400).send({
+            message: "Clothes can not be empty"
+        });
+    }
+
+    const signup = new user_signup({
+        email: req.body.email || "No cloth email", 
+        password: req.body.password,
+        confirm_password: req.body.confirm_password,
+    });
+
+    signup.save()
+    .then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Something wrong"
+        });
+    });
+};
--- Clothes/routes/clothes.js
+++ Clothes/routes/clothes.js
@@ -2,9 +2,14 @@ module.exports = (app) => {
     const clothes = require('../controller/clothes.js');
 
 
-    app.get('/', function(req, res, next) {
-        res.render('insert', { title: 'My Form' });
+    app.get('/login', function(req, res, next) {
+        res.render('login', { title: 'My Form' });
       });
 
+    app.get('/signup', function(req, res, next) {
+        res.render('signup', { title: 'My Form' });
+      });
+    
+    app.post('/signup', clothes.signup);
     app.post('/cloth',clothes.cloth);
 }
